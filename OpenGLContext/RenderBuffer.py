d56a19b1fd671c95e52c0bf2c85dc4ae80852904
ubuntunux <ubuntunux@gmail.com>
2017-09-26 23:07:54 +0900

    renderbuffer

--- OpenGLContext/RenderBuffer.py
+++ OpenGLContext/RenderBuffer.py
@@ -4,22 +4,16 @@ from OpenGL.GL.EXT.framebuffer_object import *
 
 from Common import logger
 from Utilities import GetClassName, Attributes
+from .Texture import Texture
 
 
-class RenderBuffer:
+class RenderBuffer(Texture):
     target = GL_RENDERBUFFER
 
     def __init__(self, name, **datas):
-        logger.info("Create " + GetClassName(self) + " : " + name)
-        self.name = name
-        self.width = datas.get('width', 1024)
-        self.height = datas.get('height', 1024)
-        # GL_RGBA, GL_DEPTH_COMPONENT32, GL_DEPTH24_STENCIL8
-        self.internal_format = datas.get('internal_format', GL_RGBA)
-        self.mutlisamples = datas.get('multisamples', 0)
-        self.attachment = False
-        self.using = False
-        self.attribute = Attributes()
+        Texture.__init__(self, name=name, **datas)
+
+        self.mutlisamples = datas.get('mutlisamples', 0)
 
         self.buffer = glGenRenderbuffers(1)
         glBindRenderbuffer(GL_RENDERBUFFER, self.buffer)
@@ -30,23 +24,8 @@ class RenderBuffer:
                                              self.height)
         glBindRenderbuffer(GL_RENDERBUFFER, 0)
 
-    def getAttribute(self):
-        self.attribute.setAttribute("name", self.name)
-        self.attribute.setAttribute("target", self.target)
-        self.attribute.setAttribute("width", self.width)
-        self.attribute.setAttribute("height", self.height)
-        self.attribute.setAttribute("internal_format", self.internal_format)
-        self.attribute.setAttribute("mutlisamples", self.mutlisamples)
-        return self.attribute
-
     def bind_render_buffer(self):
         glBindRenderbuffer(GL_RENDERBUFFER, self.buffer)
 
     def bind_texture(self):
         logger.error('%s RenderBuffer cannot use bind_texture method.' % self.name)
-
-    def is_attached(self):
-        return self.attachment
-
-    def set_attachment(self, attachment):
-        self.attachment = attachment
