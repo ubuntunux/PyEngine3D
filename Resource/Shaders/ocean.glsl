2a1a8f6af0f89cdc75b3454776b000fd8a5ff99d
ubuntunux <ubuntunux@gmail.com>
2018-02-23 04:41:05 +0900

    update ocean

--- Resource/Shaders/ocean.glsl
+++ Resource/Shaders/ocean.glsl
@@ -1,15 +1,15 @@
 #include "ocean_vs.glsl"
 
-
 #ifdef GL_FRAGMENT_SHADER
 layout (location = 0) in VERTEX_OUTPUT vs_output;
 layout (location = 0) out vec4 fs_output;
 
 void main() {
     vec2 uv = vs_output.tex_coord.xy;
-    float noise = texture(texture_noise, uv).x;
+    vec3 foam = texture(texture_foam, uv * uv_tiling).xyz;
 
-    fs_output.xyz = vec3(noise);
+    fs_output.xyz = foam;//* clamp(vs_output.wave_offset.y, 0.2, 1.0);
+
     fs_output.a = 1.0;
 }
 #endif // GL_FRAGMENT_SHADER
--- Resource/Shaders/ocean_vs.glsl
+++ Resource/Shaders/ocean_vs.glsl
@@ -1,17 +1,19 @@
 #include "scene_constants.glsl"
 #include "utility.glsl"
 
-#ifdef MATERIAL_COMPONENTS
-    uniform sampler2D texture_noise;
-#endif
-
 uniform float height;
 uniform vec2 grid_size;
+
+#ifdef MATERIAL_COMPONENTS
+    uniform vec2 uv_tiling;
+    uniform sampler2D texture_noise;
+    uniform sampler2D texture_foam;
+#endif
 
 struct VERTEX_OUTPUT
 {
     vec2 tex_coord;
-    vec3 position;
+    vec3 wave_offset;
 };
 
 #ifdef GL_VERTEX_SHADER
@@ -23,6 +25,20 @@ layout (location = 4) in vec2 vs_in_tex_coord;
 layout (location = 5) in vec2 offset;   // instance buffer
 
 layout (location = 0) out VERTEX_OUTPUT vs_output;
+
+vec3 GerstnerWave(vec3 world_pos, vec3 dir, float frequency, float speed, float intensity, float noise)
+{
+    speed *= TIME * frequency;
+    dir = normalize(dir);
+
+    float noise_offset = noise * 5.0;
+    intensity = intensity * (noise * 0.5 + 0.5);
+
+    float d = dot(dir, world_pos) * frequency + noise_offset;
+    float s = sin(d + speed) * intensity;
+    float c = cos(d + speed) * intensity;
+    return vec3(0.0, c, 0.0) - dir * s * 2.0;
+}
 
 
 void main()
@@ -48,9 +64,18 @@ void main()
     world_pos.y = h;
     world_pos.xyz += CAMERA_POSITION.xyz;
 
-    vec2 uv = world_pos.xz * 0.001;
-    float noise = texture(texture_noise, uv + vec2(TIME) * 0.001).x;
-    world_pos.y += sin(TIME + noise * 30.0) * 2.7;
+    vec2 uv = world_pos.xz * 0.01;
+    float noise = texture(texture_noise, uv * 0.5).x * 0.0;
+
+    vec3 wave_offset = GerstnerWave(world_pos.xyz, vec3(1.0, 0.0, 1.0), 0.05, 15.0, 8.0, noise);
+    wave_offset += GerstnerWave(world_pos.xyz, vec3(-0.2, 0.0, 1.0), 0.07, 10.5, 7.0, noise);
+    wave_offset += GerstnerWave(world_pos.xyz, vec3(0.5, 0.0, 1.0), 0.09, 18.71, 5.0, noise);
+    wave_offset += GerstnerWave(world_pos.xyz, vec3(0.7, 0.0, -0.2), 0.06, 12.31, 5.0, noise);
+
+    //wave_offset += GerstnerWave(world_pos.xyz, vec3(0.1, 0.0, 0.19), 0.432, 5.31, 1.0, noise);
+    //wave_offset += GerstnerWave(world_pos.xyz, vec3(-0.31, 0.0, -2.35), 0.65, 2.31, 0.3, noise);
+
+    world_pos.xyz += wave_offset;
 
     vec4 proj_pos = VIEW_PROJECTION * vec4(world_pos.xyz, 1.0);
     if(dist < NEAR_FAR.y)
@@ -58,20 +83,7 @@ void main()
         proj_pos.xy = mix(proj_pos.xy, vs_in_position.xz * proj_pos.w, clamp(abs(vs_in_position.xz) * 10.0 - 9.0, 0.0, 1.0));
     }
 
-    /*
-    // Instanced Grid
-    vec3 world_pos = vec3(0.0);
-    world_pos.xz += (vs_in_position.xz + offset) * grid_size;
-    world_pos.y += height;
-
-    vec2 uv = world_pos.xz * 0.01;
-    float noise = texture(texture_noise, uv + vec2(TIME) * 0.001).x;
-    world_pos.y += sin(TIME + noise * 30.0) * 0.7;
-
-    vec4 proj_pos = VIEW_PROJECTION * vec4(world_pos.xyz, 1.0);
-    */
-
-    vs_output.position = world_pos.xyz;
+    vs_output.wave_offset = wave_offset;
     vs_output.tex_coord = uv;
     gl_Position = proj_pos;
 }
