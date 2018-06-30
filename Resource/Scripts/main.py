55e53e372696def7606c3ae17f602efef23dbd75
ubuntunux <ubuntunux@gmail.com>
2018-07-01 05:07:21 +0900

    update script manager

--- Resource/Scripts/main.py
+++ Resource/Scripts/main.py
@@ -2,12 +2,23 @@ from Common import logger
 from Utilities import Singleton
 
 
-class ScriptManager(Singleton):
+class BaseScriptManager(Singleton):
     def __init__(self):
         self.core_manager = None
 
     def initialize(self, core_manager):
         self.core_manager = core_manager
+
+    def update(self, dt):
+        pass
+
+
+class ScriptManager(BaseScriptManager):
+    def __init__(self):
+        super().__init__()
+
+    def initialize(self, core_manager):
+        super().initialize(core_manager)
 
     def update(self, dt):
         pass
