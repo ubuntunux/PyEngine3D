ad65e4ab86483bae0b05d74a8658516d15724db2
ubuntunux <ubuntunux@gmail.com>
2019-02-22 01:24:32 +0900

    near plane cutting bug fix

--- Resource/Shaders/quad.glsl
+++ Resource/Shaders/quad.glsl
@@ -12,5 +12,7 @@ void main() {
     vs_output.position = vs_in_position.xyz;
     vs_output.tex_coord = vs_in_position.xy * 0.5 + 0.5;
     gl_Position = vs_in_position;
+    gl_Position.z = -1.0;
+    gl_Position.w = 1.0;
 }
 #endif // VERTEX_SHADER
