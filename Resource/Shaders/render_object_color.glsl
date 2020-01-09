52a834345a9705d4123ec4fec5a6ac85f963ef3b
ubuntunux <ubuntunux@gmail.com>
2020-01-10 01:37:55 +0900

    update

--- Resource/Shaders/render_object_color.glsl
+++ Resource/Shaders/render_object_color.glsl
@@ -2,13 +2,13 @@
 #include "scene_constants.glsl"
 #include "default_vs.glsl"
 
-uniform vec3 color;
+uniform vec4 color;
 
 #ifdef FRAGMENT_SHADER
 layout (location = 0) in VERTEX_OUTPUT vs_output;
 layout (location = 0) out vec4 fs_output;
 
 void main() {
-    fs_output = vec4(color, 0.9);
+    fs_output = color;
 }
 #endif
